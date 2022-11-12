c78660f3960be9ad40d508c9351de3b8eca3425f
abdurrahman720 <personal.abdurrahman@gmail.com>
2022-11-13 04:32:58 +0600

    compleleted the whole project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Header, Footer, Blog, Possibility, Features, WhatGPT3 } from './Containers';
+import { Article, Brand, Cta, Feature, Navbar } from './Components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <div className="gradinet__bg">
+                <Navbar></Navbar>
+                <Header></Header>
+            </div>
+            <Brand></Brand>
+            <WhatGPT3></WhatGPT3>
+            <Features></Features>
+            <Possibility></Possibility>
+            <Cta></Cta>
+            <Blog></Blog>
+            <Footer></Footer>
+
+        </div>
+    );
+};
 
 export default App;
